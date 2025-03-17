073f7dd1213208cbf1af51038fece63c0efbf078
Shiva <ShivaJaiswal@lightningtechnologies.net>
2025-03-17 13:02:38 +0530

    Changes are mades

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -98,6 +98,9 @@ export const HeroContent = () => {
 
         <motion.a
           variants={slideInFromLeft(1)}
+          href="https://drive.google.com/file/d/19NWxXoXfRAAgydFw86_5OjuFlxHTeRdE/view?usp=sharing" 
+  target="_blank"
+  rel="noopener noreferrer"
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
           Resume
